f4f3964c3ffb25eaf94a8ac70a89eed0d281e241
Henry Heino <personalizedrefrigerator@gmail.com>
2025-04-17 21:40:51 -0700

    fix: Fix issues where hidden comments could cause line numbers to overlap

--- src/contentScript/replace/replaceHTMLComments.ts
+++ src/contentScript/replace/replaceHTMLComments.ts
@@ -2,31 +2,42 @@ import { Decoration, EditorView } from '@codemirror/view';
 import { SyntaxNode } from '@lezer/common';
 import { EditorState } from '@codemirror/state';
 import makeBlockReplaceExtension from './utils/makeBlockReplaceExtension';
+import makeInlineReplaceExtension from './utils/makeInlineReplaceExtension';
 
 // CSS setup
 const hiddenCommentClass = 'cm-hidden-html-comment';
 const hideHtmlCommentsTheme = EditorView.baseTheme({
-    [`& .${hiddenCommentClass}`]: {
-        display: 'none',
-    },
+	[`& .${hiddenCommentClass}`]: {
+		// Hidden comments need to have non-zero height in order to prevent
+		// issues when line numbers are visible.
+		height: '1px',
+		display: 'inline-block',
+
+		// Hide the content of the block
+		'& > *': {
+			display: 'none',
+		},
+	},
 });
 
-const htmlCommentSpec = {
-    createDecoration: (node: SyntaxNode, state: EditorState) => {
-        // CommentBlock should be the node we're looking for
-        if (node.name === 'CommentBlock') {
-            return Decoration.mark({ class: hiddenCommentClass });
-        }
-        // Don't decorate anything else...
-        return null;
-    },
-};
+const htmlCommentSpec = (nodeName: string) => ({
+	createDecoration: (node: SyntaxNode, _state: EditorState) => {
+		// CommentBlock should be the node we're looking for
+		if (node.name === nodeName) {
+			return Decoration.mark({ class: hiddenCommentClass });
+		}
+		// Don't decorate anything else...
+		return null;
+	},
+});
 
-const hideHtmlCommentsExtension = makeBlockReplaceExtension(htmlCommentSpec);
+const hideBlockCommentsExtension = makeBlockReplaceExtension(htmlCommentSpec('CommentBlock'));
+const hideInlineCommentsExtension = makeInlineReplaceExtension(htmlCommentSpec('Comment'));
 
 const replaceHTMLComments = [
-    hideHtmlCommentsExtension,
-    hideHtmlCommentsTheme,
+	hideBlockCommentsExtension,
+	hideInlineCommentsExtension,
+	hideHtmlCommentsTheme,
 ];
 
 export default replaceHTMLComments;
